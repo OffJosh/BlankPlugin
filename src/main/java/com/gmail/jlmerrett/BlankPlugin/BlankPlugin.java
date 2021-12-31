2c0501d3bf7183258a9274cb43cd03fb8bac6531
OffJosh <67562376+OffJosh@users.noreply.github.com>
2021-12-31 14:06:02 +0000

    Update BlankPlugin.java

--- src/main/java/com/gmail/jlmerrett/BlankPlugin/BlankPlugin.java
+++ src/main/java/com/gmail/jlmerrett/BlankPlugin/BlankPlugin.java
@@ -1,15 +1,10 @@
 package com.gmail.jlmerrett.BlankPlugin;
 
-import com.gmail.jlmerrett.BlankPlugin.EventHandlers.BreakBlockEventHandler;
-import com.gmail.jlmerrett.BlankPlugin.EventHandlers.Commands;
-import com.gmail.jlmerrett.BlankPlugin.EventHandlers.PlayerJoinEventHandler;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlankPlugin extends JavaPlugin {
 
-    static BreakBlockEventHandler breakBlockEventHandler;
-    static PlayerJoinEventHandler playerJoinEventHandler;
     static Plugin plugin;
 
     @Override
@@ -24,11 +19,5 @@ public class BlankPlugin extends JavaPlugin {
 
     private void initPlugin(){
         plugin = this;
-        breakBlockEventHandler = new BreakBlockEventHandler();
-        playerJoinEventHandler = new PlayerJoinEventHandler(plugin);
-        getServer().getPluginManager().registerEvents(breakBlockEventHandler, plugin);
-        getServer().getPluginManager().registerEvents(playerJoinEventHandler, plugin);
-        this.getCommand("treecap").setExecutor(new Commands(this));
-
     }
 }
